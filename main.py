f1a8f76dd5a93ad8c7aa2dbbce01b07160b816d3
BuildTools <unconfigured@null.spigotmc.org>
2019-08-31 00:48:25 +0200

    Added Login and Password Encryption, rearranged some code, add comments, gui, multiple accounts SoonTM

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ def login():
     password = None
     log_in = input("Login: ")
     password = input("Password: ")
-    if log_in == "".join(data.keys()) and password == "".join(data.values()):
+    encrypted_log_in = "".join(data.keys())
+    encrypted_password = "".join(data.values())
+    friendly_decrypted_log_in = utils.decrypt_credential_make_friendly(encrypted_log_in)
+    friendly_decrypted_password = utils.decrypt_credential_make_friendly(encrypted_password)
+    if log_in == friendly_decrypted_log_in and password == friendly_decrypted_password:
         print("Log in successful!")
     elif log_in == "close":
         print("Closing!")
+        pass
     else:
         print("Something went wrong...\nPlease try again!")
-        # Debugging 
-        # print("".join(data.keys()))
-        # print("".join(data.values()))
         login()
 
 def register():
@@ -25,8 +27,12 @@ def register():
     password = input("Enter your Password: ")
     re_password = input("Re-Enter your Password: ")
     if password == re_password:
+        encrypted_login = utils.encrypt_credential(log_in)
+        encrypted_password = utils.encrypt_credential(password)
+        json_friendly_login = utils.make_json_friendly(encrypted_login)
+        json_friendly_password = utils.make_json_friendly(encrypted_password)
         data = {}
-        data[log_in] = password
+        data[json_friendly_login] = json_friendly_password
         utils.write_file(data_file, data)
         print("Registered!")
     else:
@@ -34,6 +40,7 @@ def register():
         register()
 
 def main():
+    utils.generate_key()
     print("Hello, would you want to Log in or Register?\n")
     choice = input()
     if choice == "Log in":
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 # utils.py
 import json
 import os
+from cryptography.fernet import Fernet
+
+key_path = "key.data"
 
 def write_file(file_path, data):
     with open(file_path, "w") as json_file:
@@ -11,5 +14,41 @@ def load_file(file_path):
         return json.load(json_file)
 
 def check_file(file_path):
-        if os.path.isfile(file_path + ".json") == False:
-                write_file(file_path, "{}")
+        if os.path.isfile(file_path) == False:
+                msg = {}
+                write_file(file_path, msg)
+
+def generate_key():
+        if os.path.isfile(key_path) == False:
+                key_data = Fernet.generate_key()
+                key_file = open("key.data", "wb")
+                key_file.write(key_data)
+                key_file.close()
+
+def load_key():
+        with open(key_path, "rb") as key:
+                bin_key = key.read()
+                return bin_key
+
+def encrypt_credential(unsafe_credential):
+        credential = unsafe_credential.encode()
+        key_data = load_key()
+        f = Fernet(key_data)
+        encrypted_credential = f.encrypt(credential)
+        return encrypted_credential
+
+def make_json_friendly(data):
+        json_friendly = data.decode("utf8").replace("'", '"')
+        return json_friendly
+
+def decrypt_credential(encrypted_credential):
+        encoded_encrypted_credential = encrypted_credential.encode()
+        key_data = load_key()
+        f = Fernet(key_data)
+        decrypted_credential = f.decrypt(encoded_encrypted_credential)
+        return decrypted_credential
+
+def decrypt_credential_make_friendly(credential):
+        decrypted_credential = decrypt_credential(credential)
+        friendly_decrypted_credential = make_json_friendly(decrypted_credential)
+        return friendly_decrypted_credential
