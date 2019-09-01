80eaeb17f13b1c4f96881d039aff8ff8fc6ca5df
BuildTools <unconfigured@null.spigotmc.org>
2019-09-01 16:23:18 +0200

    Added a Register Button - WIP

--- gui.py
+++ gui.py
@@ -25,15 +25,26 @@ def login_page():
     password_entry.config(show = "*")
     password_entry.place(relx = 0.425, rely = 0.225, relwidth = 0.485)
 
-    login_button = tk.Button(frame, text = "Login", command = lambda:test(login_entry.get()))
-    login_button.place(relx = 0.75, rely = 0.8)
+    login_button = tk.Button(frame, text = "Login", command = lambda: login(login_entry.get()) + password(password_entry.get()))
+    login_button.place(relx = 0.75, rely = 0.4)
     
-    
+    register_label = tk.Label(frame, text = "Don't have an account yet?", bg = "#C8D3D5")
+    register_label.place(relx = 0.075, rely = 0.65)
+
+    register_page_button = tk.Button(frame, text = "Register", command = lambda: register_page())
+    register_page_button.place(relx = 0.075, rely = 0.8)
     
     root.resizable(False, False)
     root.mainloop()
 
-def test(content):
-    print(content)
+def register_page():
+    pass
+
+
+def login(log_in):
+    return log_in
+def password(password):
+    return password
+
 
 login_page()
