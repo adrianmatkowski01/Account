938b1247ae91112dc3e3f7901666588160f2f31e
BuildTools <unconfigured@null.spigotmc.org>
2019-09-08 18:57:14 +0200

    Added working GUI

--- account.py
+++ account.py
@@ -1,33 +1,51 @@
 import utils
+import gui
 
+utils.generate_key()
 data_file = "data.json"
 utils.check_file(data_file)
-data = utils.load_file(data_file)
+
+def login_variable(var):
+    global login_entry_variable
+    login_entry_variable = var
+
+def password_variable(var):
+    global password_entry_variable
+    password_entry_variable = var
+
+def register_login_variable(var):
+    global login_register_entry_variable
+    login_register_entry_variable = var
+
+def register_password_variable(var):
+    global password_register_entry_variable
+    password_register_entry_variable = var
+
+def register_repassword_variable(var):
+    global repassword_register_entry_variable
+    repassword_register_entry_variable = var
 
 def login():
     # Login function
-    log_in = None
-    password = None
-    log_in = input("Login: ")
-    password = input("Password: ")
+    log_in = login_entry_variable
+    password = password_entry_variable
+    data = utils.load_file(data_file)
     encrypted_log_in = "".join(data.keys())
     encrypted_password = "".join(data.values())
     friendly_decrypted_log_in = utils.decrypt_credential_make_friendly(encrypted_log_in)
     friendly_decrypted_password = utils.decrypt_credential_make_friendly(encrypted_password)
     if log_in == friendly_decrypted_log_in and password == friendly_decrypted_password:
-        print("Log in successful!")
-    elif log_in == "close":
-        print("Closing!")
-        pass
+        # Login success 
+        gui.label_text_variable = "Login successful"
     else:
-        print("Something went wrong...\nPlease try again!")
-        login()
-
+        # Login bad
+        gui.label_text_variable = "Wrong Login or Password!"
+    
 def register():
     # Register function
-    log_in = input("Enter your Login: ")
-    password = input("Enter your Password: ")
-    re_password = input("Re-Enter your Password: ")
+    log_in = login_register_entry_variable
+    password = password_register_entry_variable
+    re_password = repassword_register_entry_variable
     if password == re_password:
         encrypted_login = utils.encrypt_credential(log_in)
         encrypted_password = utils.encrypt_credential(password)
@@ -36,7 +54,6 @@ def register():
         data = {}
         data[json_friendly_login] = json_friendly_password
         utils.write_file(data_file, data)
-        print("Registered!")
+        gui.label_text_variable = "Registered!"
     else:
-        print("Your Passwords dont match!\nTry again!")
-        register()
+        gui.label_text_variable = "Your Passwords dont match!\nTry again!"
--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import utils
 import account
 
-def main():
-    # Main funtion
-    utils.generate_key()
-    print("Hello, would you want to Log in or Register?\n")
-    choice = input()
-    if choice == "Log in":
-        account.login()
-    elif choice == "Register":
-        account.register()
-    elif choice == "close":
-        print("Closing!")
-
 if __name__ == "__main__":
-    main()
+    pass
